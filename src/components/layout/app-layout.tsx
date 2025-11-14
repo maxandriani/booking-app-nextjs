07505e48bcb424d71de146e25d13ad6bcdd60932
Maxmiliano <max.andriani@gmail.com>
2025-11-14 00:25:38 -0300

    feat: Added view transitions initial support

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,13 +1,16 @@
+import { ViewTransition } from "react";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/layout/app-sidebar";
 
 export default function AppLayout({ children }: React.PropsWithChildren) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                {children}
-            </SidebarInset>
-        </SidebarProvider>
+        <ViewTransition name="app-layout" enter="slide-in">
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    {children}
+                </SidebarInset>
+            </SidebarProvider>
+        </ViewTransition>
     )
 }
